7d5fbdbe6349bc5b42b205f09a76037b61fec0cc
akshangolla <shivsaicool@gmail.com>
2024-09-30 14:40:00 -0500

    completed milestone 2 of lab 3!

--- app/src/main/java/com/cs407/lab3/milestone2/MainActivity.kt
+++ app/src/main/java/com/cs407/lab3/milestone2/MainActivity.kt
@@ -1,20 +1,30 @@
 package com.cs407.lab3.milestone2
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val fragmentManager = supportFragmentManager
+
+        findViewById<Button>(R.id.firstFragmentButton).setOnClickListener {
+            fragmentManager.beginTransaction()
+                .replace(R.id.fragmentContainerView, FirstFragment::class.java, null)
+                .setReorderingAllowed(true)
+                .addToBackStack("showing First")
+                .commit()
+        }
+
+        findViewById<Button>(R.id.secondFragmentButton).setOnClickListener {
+            fragmentManager.beginTransaction()
+                .replace(R.id.fragmentContainerView, SecondFragment::class.java, null)
+                .setReorderingAllowed(true)
+                .addToBackStack("showing Second")
+                .commit()
         }
     }
 }
